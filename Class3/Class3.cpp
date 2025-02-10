207763e89068f35d7dae598e53816500aac33efe
Anton <antoha357357@gmail.com>
2025-02-10 14:58:25 +0300

    Add project files.

---
+++ Class3/Class3.cpp
@@ -0,0 +1,42 @@
+/* Подсчет кол-ва слов в строке */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int countWords(string str);
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    
+    string str;
+
+    cout << "Введите строку: " << endl;
+    getline(cin, str);
+
+    int res = countWords(str);
+
+    cout << "Результа: " << res << endl;
+
+    return 0;
+
+}
+
+int countWords(string str) {
+
+    int count = 0;
+    bool inWord = false;
+
+    for (char c : str) {
+        if (isspace(c)) {
+            inWord = false;
+        }
+        else if (!inWord) {
+            count++;
+            inWord = true;
+        }
+    }
+
+    return count;
+}
